33f3fcfd2861e880786e238f75ed4862b4fa3b99
Pranav Vashi <neobuddy89@gmail.com>
2018-01-20 00:27:08 +0530

    sdm845-common: Compile libhwui in perf mode
    Change-Id: I598de494c381848ac0d384214c87ae1305ecec61
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -31,6 +31,9 @@ TARGET_USES_64_BIT_BINDER := true
 TARGET_BOOTLOADER_BOARD_NAME := sdm845
 TARGET_NO_BOOTLOADER := true
 
+# Compile libhwui in performance mode
+HWUI_COMPILE_FOR_PERF := true
+
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0xA84000 androidboot.hardware=qcom androidboot.console=ttyMSM0 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 service_locator.enable=1 swiotlb=2048 androidboot.configfs=true loop.max_part=16 androidboot.usbcontroller=a600000.dwc3
