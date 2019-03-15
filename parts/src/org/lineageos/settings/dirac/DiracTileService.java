607bd56a457130dc28a32d1fd664162ad217b10d
HarukeyUA <inazarrusnak@gmail.com>
2019-03-15 18:28:02 +0200

    sdm845-common: parts: add QS tile for Dirac
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

---
+++ parts/src/org/lineageos/settings/dirac/DiracTileService.java
@@ -0,0 +1,38 @@
+package org.lineageos.settings.dirac;
+
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+
+public class DiracTileService extends TileService {
+
+    @Override
+    public void onStartListening() {
+
+        boolean enhancerEnabled = DiracUtils.isDiracEnabled(getQsTile());
+
+        Tile tile = getQsTile();
+        if (enhancerEnabled) {
+            tile.setState(Tile.STATE_ACTIVE);
+        } else {
+            tile.setState(Tile.STATE_INACTIVE);
+        }
+
+        tile.updateTile();
+
+        super.onStartListening();
+    }
+
+    @Override
+    public void onClick() {
+        Tile tile = getQsTile();
+        if (DiracUtils.isDiracEnabled()) {
+            DiracUtils.setEnabled(false);
+            tile.setState(Tile.STATE_INACTIVE);
+        } else {
+            DiracUtils.setEnabled(true);
+            tile.setState(Tile.STATE_ACTIVE);
+        }
+        tile.updateTile();
+        super.onClick();
+    }
+}
