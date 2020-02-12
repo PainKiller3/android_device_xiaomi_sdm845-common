a1ddf37acd103ed0d16fcb20037071c8bc2d7ff2
XsaviXander <22182013+XsaviXander@users.noreply.github.com>
2020-02-12 01:51:30 +0000

    sdm845-common: Enable Freeform Multiwindow
    Change-Id: I82965ce0c285ef52bc44b0745ec7c2b0007ec66d
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -164,6 +164,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.3-service.clearkey
 
+# Freeform Multiwindow
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
+
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl-qti \
