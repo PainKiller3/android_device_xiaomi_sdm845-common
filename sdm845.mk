304ca7ab3722310fb7990b83bf00991725cba9c6
Murthy Nidadavolu <mnssr@codeaurora.org>
2020-03-03 11:03:06 +0530

    sdm845-common: Remove DRM Widevine and Clearkey HALs
    DRM Widevine and Clearkey HAL entries moved securemsm specific
    makefiles complying FR53051.
    
    Change-Id: I5330942888f9de224175fd31123ec76b99aaa2b1

--- sdm845.mk
+++ sdm845.mk
@@ -162,8 +162,6 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl:64 \
-    android.hardware.drm@1.0-service \
     android.hardware.drm@1.3-service.clearkey
 
 # Health
