6db98cc71e8f11f021044a96667e0d6a1ebf4609
Pranav Vashi <neobuddy89@gmail.com>
2020-05-21 21:55:10 +0530

    sdm845-common: parts: Switch back to default thermal profile when screen off
    Change-Id: I72aae3c1fbb13f44ff7a637ba9ad70cf451d71a2
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- parts/src/org/lineageos/settings/thermal/ThermalUtils.java
+++ parts/src/org/lineageos/settings/thermal/ThermalUtils.java
@@ -19,11 +19,12 @@ package org.lineageos.settings.thermal;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.FileUtils;
 import android.os.UserHandle;
 import android.util.Log;
 
 import androidx.preference.PreferenceManager;
+
+import org.lineageos.settings.utils.FileUtils;
 
 import java.io.IOException;
 
@@ -137,6 +138,10 @@ public final class ThermalUtils {
         return state;
     }
 
+    protected void setDefaultThermalProfile() {
+        FileUtils.writeLine(THERMAL_SCONFIG, THERMAL_STATE_DEFAULT);
+    }
+
     protected void setThermalProfile(String packageName) {
         String value = getValue();
         String modes[];
@@ -159,11 +164,6 @@ public final class ThermalUtils {
                 state = THERMAL_STATE_STREAMING;
             }
         }
-
-        try {
-            FileUtils.stringToFile(THERMAL_SCONFIG, state);
-        } catch (IOException e) {
-            Log.e(TAG, "Failed to write to " + THERMAL_SCONFIG, e);
-        }
+        FileUtils.writeLine(THERMAL_SCONFIG, state);
     }
 }
