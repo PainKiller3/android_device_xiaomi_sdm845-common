42b449b57ddd1499254d4170d1bdd6a2236660e3
PIPIPIG233666 <pig.priv@gmail.com>
2020-08-06 09:37:30 +0530

    sdm845-common: Build common thermal HAL
    Change-Id: Idb15ef0aacf22dcfb2b21feedd08f27812be1df8

--- sdm845.mk
+++ sdm845.mk
@@ -314,9 +314,7 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service \
-    thermal.sdm845
+    android.hardware.thermal@2.0-service.xiaomi
 
 # Tracing
 PRODUCT_PACKAGES += \
