1ddd2c717129af57ede84549555b160b497569a0
Saikiran <bjsaikiran@gmail.com>
2020-08-28 13:07:03 +0530

    sdm845-common: Build ldac libs
    Change-Id: I6c3b6092d0b87a7ae8838c8df564450bb08357fd
    Signed-off-by: Saikiran <bjsaikiran@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -104,6 +104,8 @@ PRODUCT_PACKAGES += \
     audio.bluetooth.default \
     BluetoothQti \
     libbthost_if \
+    libldacBT_enc \
+    libldacBT_abr \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor
 
