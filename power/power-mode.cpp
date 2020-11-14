7cd8789531845990626c64cee32d1aede645c60c
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-11-14 13:29:22 +0530

    sdm845-common: power: Implement extension for kernelspace battery saver
    * This powerhal extension will process LOW_POWER (battery saver)
      hint and enable/disable our dedicated kernelspace battery saver
      automatically, for some legit power savings when user demands it.
    
    Change-Id: I54bed563be4e65035ab553aba3baed4247ad8720
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- power/power-mode.cpp
+++ power/power-mode.cpp
@@ -56,6 +56,8 @@ int open_ts_input() {
 }
 }  // anonymous namespace
 
+#define BATTERY_SAVER_NODE "/sys/module/battery_saver/parameters/enabled"
+
 namespace aidl {
 namespace google {
 namespace hardware {
@@ -71,6 +73,9 @@ using ::aidl::android::hardware::power::Mode;
 bool isDeviceSpecificModeSupported(Mode type, bool* _aidl_return) {
     switch (type) {
         case Mode::DOUBLE_TAP_TO_WAKE:
+            *_aidl_return = true;
+            return true;
+        case Mode::LOW_POWER:
             *_aidl_return = true;
             return true;
         default:
@@ -94,6 +99,9 @@ bool setDeviceSpecificMode(Mode type, bool enabled) {
             write(fd, &ev, sizeof(ev));
             close(fd);
         }
+            return true;
+        case Mode::LOW_POWER:
+            ::android::base::WriteStringToFile(enabled ? "Y" : "N", BATTERY_SAVER_NODE, true);
             return true;
         default:
             return false;
