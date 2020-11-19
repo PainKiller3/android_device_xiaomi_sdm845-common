5e317aad5edfa5cfad47b3321b2b2c663c376cd0
Henrique Silva <jhenrique09.mcz@hotmail.com>
2020-11-19 21:50:18 +0530

    sdm845-common: Don't build LiveDisplay
    Change-Id: I2a93f55f0b7a4a4992a475287a40b1f8a15b0cc4

--- sdm845.mk
+++ sdm845.mk
@@ -203,10 +203,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_sdm845
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
-
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail \
